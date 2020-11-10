05e397adcb9dfda1b1c113b66496977a925fb971
kedark2 <kedar.kanel@gmail.com>
2020-11-10 11:33:24 +0200

    gradebook-web

---
+++ training-workspace/modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -0,0 +1,31 @@
+package com.liferay.training.gradebook.web.portlet;
+
+import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kedar
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + GradebookPortletKeys.Gradebook,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user",
+		"com.liferay.portlet.display-category=category.training",
+		"com.liferay.portlet.instanceable=false"
+	},
+	service = Portlet.class
+)
+public class GradebookPortlet extends MVCPortlet {
+}
